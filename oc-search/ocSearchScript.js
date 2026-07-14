3b4a0dbcd2925bf32a7025106d0e2db009d3e429
flint-nsteel <flintkuznetsov@gmail.com>
2026-07-14 10:48:38 +0200

    Update visibility for True/False filters
    Upon Search Reset, fragmentSelect and sexSelect will be hidden along with the rest of the sub-filters.

--- oc-search/ocSearchScript.js
+++ oc-search/ocSearchScript.js
@@ -112,18 +112,18 @@ function reset(){
         document.getElementById(`${document.getElementById("searchSelect").value}`).style.visibility = "hidden"
         for (let i = 0; i < subFilterList.length; i++) {
             document.getElementById(`${subFilterList[i]}`).style.visibility = "hidden"
+            document.getElementById("fragmentSelect").style.visibility = "hidden"
+            document.getElementById("sexSelect").style.visibility = "hidden"
         }
-        // document.getElementById("fragmentSelect").style.visibility = "hidden"
         // document.getElementById("conservation-material").style.visibility = "hidden"
         // document.getElementById("conservation-action").style.visibility = "hidden"
-        // document.getElementById('searchButton').style.visibility = "hidden"
         // document.getElementById("preserved").style.visibility = "hidden"
         // document.getElementById("proximal-fused").style.visibility = "hidden"
         // document.getElementById("distal-fused").style.visibility = "hidden"
         // document.getElementById("side").style.visibility = "hidden"
         // document.getElementById("age-category").style.visibility = "hidden"
         // document.getElementById("skeletal-area").style.visibility = "hidden"
-        // document.getElementById("sexSelect").style.visibility = "hidden"
+        // document.getElementById('searchButton').style.visibility = "hidden"
         // document.getElementById("resetButton").style.visibility = "hidden"
     }
 
